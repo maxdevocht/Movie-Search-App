747bbb15869f8ad990575f898eb4aeb4e74fe3bf
Max de Vocht <maxdevocht@hotmail.com>
2024-05-31 11:26:35 +0200

    project setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div className="my-0 mx-auto max-w-5xl p-10">
+      <h1 className="text-[2rem] text-center">React Movie Search</h1>
+    </div>
+  )
+}
+
+export default Main

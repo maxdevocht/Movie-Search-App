2ebee737ac262a4f64936f301c1491ec0e2de3e6
Max de Vocht <maxdevocht@hotmail.com>
2024-05-31 11:44:17 +0200

    Update SearchMovie component
    added styles

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -1,9 +1,9 @@
 const SearchMovie = () => {
   return (
-    <form action="">
-      <label htmlFor="">Movie Name</label>
-      <input type="text" name="query" placeholder="Jurrasic Park"/>
-      <button type="submit">Search</button>
+    <form className="grid p-4" action="">
+      <label className="text-[1.2rem] mb-[0.2rem]" htmlFor="">Movie Name</label>
+      <input className="text-[1.6rem] py-[0.5rem] px-[1rem] border rounded-xl mb-[1rem]" type="text" name="query" placeholder="Jurrasic Park"/>
+      <button className="bg-[#333] text-white py-[1rem] px-[2rem] border rounded-xl text-[1.4rem]cursor-pointer" type="submit">Search</button>
     </form>
   )
 }
